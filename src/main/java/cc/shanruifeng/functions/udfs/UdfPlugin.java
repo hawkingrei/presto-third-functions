ece9a7254a078d4c6019d4f699b5fc1578b3d4f2
真人 <weizhen.wang@duitang.com>
2016-11-23 16:41:02 +0800

    feat: 打开可用，关闭非可用

--- src/main/java/cc/shanruifeng/functions/udfs/UdfPlugin.java
+++ src/main/java/cc/shanruifeng/functions/udfs/UdfPlugin.java
@@ -5,7 +5,16 @@ import com.google.common.collect.ImmutableSet;
 
 import java.util.Set;
 
-import cc.shanruifeng.functions.udfs.scalar.json.JsonArrayExtractFunction;
+import cc.shanruifeng.functions.udfs.scalar.IsNullFunctions;
+import cc.shanruifeng.functions.udfs.scalar.array.ArrayValueCountFunction;
+import cc.shanruifeng.functions.udfs.scalar.date.ChinaTypeOfDayFunction;
+import cc.shanruifeng.functions.udfs.scalar.date.DayOfWeekFunctions;
+import cc.shanruifeng.functions.udfs.scalar.date.ZodiacSignFunctions;
+import cc.shanruifeng.functions.udfs.scalar.geographic.GeographicFunctions;
+import cc.shanruifeng.functions.udfs.scalar.map.MapValueCountFunction;
+import cc.shanruifeng.functions.udfs.scalar.string.ChinaIdCardFunctions;
+import cc.shanruifeng.functions.udfs.scalar.string.ChineseToPinYinFunctions;
+import cc.shanruifeng.functions.udfs.scalar.string.HashFunctions;
 
 /**
  * @author ruifeng.shan
@@ -18,18 +27,18 @@ public class UdfPlugin implements Plugin {
     public Set<Class<?>> getFunctions(){
 
         return ImmutableSet.<Class<?>>builder()
-            .add(JsonArrayExtractFunction.class)
-//            .add(HashFunctions.class)
-//            .add(IsNullFunctions.class)
-//            .add(MapValueCountFunction.class)
+//            .add(JsonArrayExtractFunction.class)
+            .add(HashFunctions.class)
+            .add(IsNullFunctions.class)
+            .add(MapValueCountFunction.class)
 //            .add(ArrayUnionFunction.class)
-//            .add(ArrayValueCountFunction.class)
-//            .add(ChinaTypeOfDayFunction.class)
-//            .add(DayOfWeekFunctions.class)
-//            .add(ZodiacSignFunctions.class)
-//            .add(GeographicFunctions.class)
-//            .add(ChinaIdCardFunctions.class)
-//            .add(ChineseToPinYinFunctions.class)
+            .add(ArrayValueCountFunction.class)
+            .add(ChinaTypeOfDayFunction.class)
+            .add(DayOfWeekFunctions.class)
+            .add(ZodiacSignFunctions.class)
+            .add(GeographicFunctions.class)
+            .add(ChinaIdCardFunctions.class)
+            .add(ChineseToPinYinFunctions.class)
             .build();
     }
 
