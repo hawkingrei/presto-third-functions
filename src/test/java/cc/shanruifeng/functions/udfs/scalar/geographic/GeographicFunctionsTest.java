8566d88d79d618cdeb7f015310e8b9e3b9209b41
真人 <weizhen.wang@duitang.com>
2016-12-11 18:23:48 +0800

    feat: test gcj_to_bd

--- src/test/java/cc/shanruifeng/functions/udfs/scalar/geographic/GeographicFunctionsTest.java
+++ src/test/java/cc/shanruifeng/functions/udfs/scalar/geographic/GeographicFunctionsTest.java
@@ -1,7 +1,16 @@
 package cc.shanruifeng.functions.udfs.scalar.geographic;
 
+import com.alibaba.fastjson.JSON;
 import com.facebook.presto.metadata.FunctionListBuilder;
+
+import io.airlift.slice.Slice;
+
 import org.junit.Test;
+
+import java.util.HashMap;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author ruifeng.shan
@@ -9,9 +18,20 @@ import org.junit.Test;
  * @time 15:08
  */
 public class GeographicFunctionsTest {
-    @Test
-    public void testFunctionCreate() throws Exception {
-        FunctionListBuilder builder = new FunctionListBuilder();
-        builder.scalars(GeographicFunctions.class);
-    }
+  @Test
+  public void testFunctionCreate() throws Exception {
+    FunctionListBuilder builder = new FunctionListBuilder();
+    builder.scalars(GeographicFunctions.class);
+  }
+
+  @Test
+  public void test_gcj_to_bd() throws Exception {
+    Slice gcj = GeographicFunctions.GCJ02ToBD09(new Double(22.499374931276), new Double(113.9431826306));
+    HashMap<String, Double> testresult = new HashMap<String, Double>();
+    testresult.put("lng", 113.94974983525127);
+    testresult.put("lat", 22.505037963671622);
+    String resesult = JSON.toJSONString(testresult);
+
+    assertTrue(gcj.toString(UTF_8).equals(resesult.toString()));
+  }
 }
